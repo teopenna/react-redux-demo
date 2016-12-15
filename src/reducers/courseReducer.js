09d5594ba7a2200065dc9294d199b114cb7c77d6
teopenna <penna.matteo@gmail.com>
2016-12-15 23:36:49 +0100

    Adding course functions

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,6 +6,18 @@ export default function courseReducer(state = initialState.courses, action) {
         [types.LOAD_COURSES_SUCCESS]: function() {
             return action.courses;
         },
+        [types.CREATE_COURSE_SUCCESS]: function() {
+            return [
+                ...state,
+                Object.assign({}, action.course)
+            ];
+        },
+        [types.UPDATE_COURSE_SUCCESS]: function() {
+            return [
+                ...state.filter(course => course.id !== action.course.id),
+                Object.assign({}, action.course)
+            ];
+        },
         'default': function() {
             return state;
         }
