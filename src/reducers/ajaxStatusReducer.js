d87d95a0554890d9b8f8680143470a534b0169a4
teopenna <penna.matteo@gmail.com>
2017-02-19 23:30:40 +0100

    Adding preloader

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,15 +1,25 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
+function actionTypeEndsInSuccess(type) {
+    return type.substring(type.length - 8) === '_SUCCESS';
+}
+
 export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
     const actionFunctions = {
         [types.BEGIN_AJAX_CALL]: function() {
             return state + 1;
         },
+        'actionTypeEndsInSuccess': function() {
+            return state - 1;
+        },
         'default': function() {
             return state;
         }
     };
-
+    
+    if (actionTypeEndsInSuccess(action.type)) {
+        return actionFunctions['actionTypeEndsInSuccess'];
+    }
     return (actionFunctions[action.type] || actionFunctions['default'])();
 }
